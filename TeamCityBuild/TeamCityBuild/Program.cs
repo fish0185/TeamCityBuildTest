f0b2046a1ef42ec1ed1d3c77ec95b1e2aaf4fbb1
Gary.Yu <Gary.Yu@zipmoney.com.au>
2017-06-06 17:34:54 +1000

    chore

--- TeamCityBuild/TeamCityBuild/Program.cs
+++ TeamCityBuild/TeamCityBuild/Program.cs
@@ -11,6 +11,7 @@ namespace TeamCityBuild
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            throw new Exception("test");
         }
     }
 }
